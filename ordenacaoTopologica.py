efa025d6089778d9a7ac91fbdc618f563fbb7407
MatheusFBBueno <matfelipe2012@gmail.com>
2022-02-05 16:03:27 -0300

    Update dirigido3.txt and ordenacaoTopologica.py

--- ordenacaoTopologica.py
+++ ordenacaoTopologica.py
@@ -1,36 +1,40 @@
 from Grafo import Grafo
 from lerEntrada import grafoDeEntrada
-global tempo
+
 
 
 def ordenacaoTopologica(gfo: Grafo):
-
-    visitados_c = {vert: False for vert in gfo.vertices}
-    tempo_t = {vert: float('inf') for vert in gfo.vertices}
-    tempo_f = {vert: float('inf') for vert in gfo.vertices}
+    visitados_c = {vert[0]: False for vert in gfo.vertices}
+    tempo_t = {vert[0]: float('inf') for vert in gfo.vertices}
+    tempo_f = {vert[0]: float('inf') for vert in gfo.vertices}
 
     tempo = 0
 
     ordenado = []
     for vert in gfo.vertices:
-        if not visitados_c[vert]:
+        if not visitados_c[vert[0]]:
             dfs_visit_ot(gfo, vert, visitados_c, tempo_t, tempo_f, tempo, ordenado)
 
+    string_formatada = ''
+    for item in ordenado:
+        string_formatada += str(item)+' -> '
+    string_formatada = string_formatada.strip(' -> ')
+    print(string_formatada)
     return ordenado
 
 
 def dfs_visit_ot(gfo, vert, visitados_c, tempo_t, tempo_f, tempo, ordenado):
-    visitados_c[vert] = True
+    visitados_c[vert[0]] = True
     tempo += 1
-    tempo_t[vert] = tempo
-    temp_vertices = list(filter(lambda x: x != vert, gfo.vertices))
-    for vert1 in temp_vertices:
-        if not visitados_c[vert1]:
-            dfs_visit_ot(gfo, vert, visitados_c, tempo_t, tempo_f, tempo, ordenado)
+    tempo_t[vert[0]] = tempo
+    for indice in gfo.vizinhos(vert[0]):
+        vert1 = gfo.vertices[indice-1]
+        if not visitados_c[vert1[0]]:
+            dfs_visit_ot(gfo, vert1, visitados_c, tempo_t, tempo_f, tempo, ordenado)
 
     tempo += 1
     tempo_f[vert] = tempo
-    ordenado.insert(0, vert)
+    ordenado.insert(0, vert[1])
 
 
 if __name__ == "__main__":
