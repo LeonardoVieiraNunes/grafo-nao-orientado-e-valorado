154f1f891991914ea726d5a579236cddfe1e9b05
MatheusFBBueno <matfelipe2012@gmail.com>
2022-02-08 17:52:58 -0300

    Update ordenacaoTopologica.py

--- ordenacaoTopologica.py
+++ ordenacaoTopologica.py
@@ -1,43 +1,45 @@
 from Grafo import Grafo
 from lerEntrada import grafoDeEntrada
 
+class OrdenacaoTopologica():
+    def __init__(self):
+        self.tempo = 0
+
+    def ordenacaoTopologica(self,gfo: Grafo):
+        visitados_c = {vert[0]: False for vert in gfo.vertices}
+        tempo_t = {vert[0]: float('inf') for vert in gfo.vertices}
+        tempo_f = {vert[0]: float('inf') for vert in gfo.vertices}
 
 
-def ordenacaoTopologica(gfo: Grafo):
-    visitados_c = {vert[0]: False for vert in gfo.vertices}
-    tempo_t = {vert[0]: float('inf') for vert in gfo.vertices}
-    tempo_f = {vert[0]: float('inf') for vert in gfo.vertices}
 
-    tempo = 0
+        ordenado = []
+        for vert in gfo.vertices:
+            if not visitados_c[vert[0]]:
+                self.dfs_visit_ot(gfo, vert, visitados_c, tempo_t, tempo_f, ordenado)
 
-    ordenado = []
-    for vert in gfo.vertices:
-        if not visitados_c[vert[0]]:
-            dfs_visit_ot(gfo, vert, visitados_c, tempo_t, tempo_f, tempo, ordenado)
-
-    string_formatada = ''
-    for item in ordenado:
-        string_formatada += str(item)+' -> '
-    string_formatada = string_formatada.strip(' -> ')
-    print(string_formatada)
-    return ordenado
+        string_formatada = ''
+        for item in ordenado:
+            string_formatada += str(item)+' -> '
+        string_formatada = string_formatada.strip(' -> ')
+        print(string_formatada)
+        return ordenado
 
 
-def dfs_visit_ot(gfo, vert, visitados_c, tempo_t, tempo_f, tempo, ordenado):
-    visitados_c[vert[0]] = True
-    tempo += 1
-    tempo_t[vert[0]] = tempo
-    for indice in gfo.vizinhos(vert[0]):
-        vert1 = gfo.vertices[indice-1]
-        if not visitados_c[vert1[0]]:
-            dfs_visit_ot(gfo, vert1, visitados_c, tempo_t, tempo_f, tempo, ordenado)
+    def dfs_visit_ot(self,gfo, vert, visitados_c, tempo_t, tempo_f, ordenado):
+        visitados_c[vert[0]] = True
+        self.tempo += 1
+        tempo_t[vert[0]] = self.tempo
+        for indice in gfo.vizinhos(vert[0]):
+            vert1 = gfo.vertices[indice-1]
+            if not visitados_c[vert1[0]]:
+                self.dfs_visit_ot(gfo, vert1, visitados_c, tempo_t, tempo_f, ordenado)
 
-    tempo += 1
-    tempo_f[vert] = tempo
-    ordenado.insert(0, vert[1])
+        self.tempo += 1
+        tempo_f[vert] = self.tempo
+        ordenado.insert(0, vert[1])
 
 
 if __name__ == "__main__":
     g1 = grafoDeEntrada("casos-de-teste/dirigido2.txt", isDirigido=True)
-    a = ordenacaoTopologica(g1)
-    print(a)
+    ord = OrdenacaoTopologica()
+    ord.ordenacaoTopologica(g1)
